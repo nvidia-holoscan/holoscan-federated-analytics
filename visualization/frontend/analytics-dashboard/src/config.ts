d9cd7c44f8983976c35267984c58a03e78a117c5
Arun Patole <apatole@nvidia.com>
2025-01-08 00:19:34 +0530

    Add initial version of analytics dashboard
    Signed-off-by: Arun Patole <apatole@nvidia.com>

--- visualization/frontend/analytics-dashboard/src/config.ts
+++ visualization/frontend/analytics-dashboard/src/config.ts
@@ -5,7 +5,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -14,8 +14,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
-import { redirect } from 'next/navigation';
+export const AppNames: Map<string, string> = new Map([
+    ["multiai_ultrasound", "MultiAI Ultrasound"],
+    ["out_of_body_detection", "Endoscopy Out of Body Detection"],
+    ["endoscopy_tool_tracking", "Endoscopy Tool Tracking"],
+    ["multiai_endoscopy", "MultiAI Endoscopy"],
+    ["tao_peoplenet", "TAO PeopleNet"]
+]);
 
-export default function Home() {
-  redirect('/apps');
-}
+export const IconNames: Map<string, string> = new Map([
+    ["multiai_ultrasound", "multiai_ultrasound.png"],
+    ["out_of_body_detection", "endoscopy_out_of_body_detection.png"],
+    ["endoscopy_tool_tracking", "endoscopy_tool_tracking.png"],
+    ["multiai_endoscopy", "endoscopy_out_of_body_detection.png"],
+]);
